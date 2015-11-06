ce544c310656bf531840c1e803bee81874c98f15
747823 <3pipta@gmail.com>
2015-11-05 19:40:20 -0500

    stuff

--- server.js
+++
@@ -1,9 +0,0 @@
-
-// Load some objects
-var UserList = require("./app/userlist");
-var GameList = require('./app/gamelist');
-
-// Start the server
-var Server = require('./app/server');
-
-console.log("ready");
