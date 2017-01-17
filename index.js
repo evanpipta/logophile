7ceea27a31971726cc12c7d7d7f1ff4fbfb65cab
Evan <3pipta@gmail.com>
2017-01-16 23:25:09 -0500

    added temporary eslint config

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 
 // Load some objects
-var UserList = require("./app/userlist");
+var UserList = require('./app/userlist');
 var GameList = require('./app/gamelist');
 
 // Start the server
 var Server = require('./app/server');
 
-console.log("ready");
+console.log('ready');
+
